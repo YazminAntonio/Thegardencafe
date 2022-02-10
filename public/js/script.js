5ccd0f025380c0b18921bf9d8c88e0bd06936a50
Miriam Y Antonio <63122270+YazminAntonio@users.noreply.github.com>
2022-02-10 14:30:35 +0700

    include the button for shopping cart

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,13 @@
-// let item = localStore.getItem(‘item’)
-// if(item){
-// $(‘<li>${item}</li>’).appendTo(‘ul’)
-// }
-// $(‘input’).blur(e) => {
-// $(‘<li>${target.value}</li>’).appendTo(‘ul’)
-// localStorage.seItem(‘item’, e.target.value)
-// }
-
-// func(id)
-// localStorage
-// const addItems = item => {
-//   let items = document.getElementById({ item })
-//   items.forEach((n, i) => {
-//     return addItems
-//   })
-// }
-
 const order = item => {
   // let items = document.findById({ items })
   let list = JSON.parse(localStorage.getItem('list')) || []
   list.push(item)
   localStorage.setItem('list', JSON.stringify(list))
   console.log(list)
-}
-//the legth of the array
-// funtion takes id go localstore and find the item
-// return cart
-const addItems = order => {
+  if (localStorage.getItem('list') === null) {
+    console.log(0)
+  } else return
   console.log(order.lenght)
 }
-
-// let shoppingcart = []
-// item.forEach(e => {
-//   shoppingcart.push(e)
-// })
-// return shoppingcart
-//
-// const sayHello = () => {
-//   console.log('hello')
-// }
-// sayHello()
+//the legth of the array
+console.log(order.lengt)
