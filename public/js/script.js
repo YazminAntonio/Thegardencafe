7009844455ca802d9c9956376b6576d1091039da
Miriam Y Antonio <63122270+YazminAntonio@users.noreply.github.com>
2022-02-16 15:11:58 +0700

    create a function for the items that are the same in the list

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,18 @@
 const showCartContent = () => {
-  // get list
-  let list = JSON.parse(localStorage.getItem('list')) || []
-  // put list length in button
-  let numberItems = list.length
-  document.getElementById('cart').innerHTML = numberItems
-  // put list of ids inside form
-  //let shoppingcartForm = document.querySelector('#shoppingcartForm')
-  let itemsList = document.querySelector('#itemsList')
-  list.forEach(e => {
-    itemsList.innerHTML += `<input type="hidden" name="items" value="${e}" />`
-  })
+  let cart = document.getElementById('cart')
+  if (cart) {
+    // get list
+    let list = JSON.parse(localStorage.getItem('list')) || []
+    // put list length in button
+    let numberItems = list.length
+    document.getElementById('cart').innerHTML = numberItems
+    // put list of ids inside form
+    //let shoppingcartForm = document.querySelector('#shoppingcartForm')
+    let itemsList = document.querySelector('#itemsList')
+    list.forEach(e => {
+      itemsList.innerHTML += `<input type="hidden" name="items" value="${e}" />`
+    })
+  }
 }
 const order = item => {
   let list = JSON.parse(localStorage.getItem('list')) || []
@@ -17,16 +20,33 @@ const order = item => {
   localStorage.setItem('list', JSON.stringify(list))
   showCartContent()
 }
-
 showCartContent()
 
 const qtyOfItems = () => {
   let list = JSON.parse(localStorage.getItem('list')) || []
-  let countItems = {}
-  list.forEach(function(listInside) {
-    listInside.forEach(function(valor) {
-      countItems[valor] = (countItems[valor] || 0) + 1
-    })
-  })
-  console.log(countItems)
+  //console.log(list)
+  list.sort()
+  let unicosElementos = []
+  let almacenadorDeVecesRepetidos = []
+  let contador = 1
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i + 1] === list[i]) {
+      contador++
+    } else {
+      unicosElementos.push(list[i])
+      almacenadorDeVecesRepetidos.push(contador)
+      contador = 1
+    }
+  }
+  for (let j = 0; j < unicosElementos.length; j++) {
+    console.log(
+      'El valor ' +
+        unicosElementos[j] +
+        'se repite ' +
+        almacenadorDeVecesRepetidos[j]
+    )
+  }
 }
+
+qtyOfItems()
